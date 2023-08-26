8d74c29e9aa3210b5c1c6d90c7fe9a124817c6f5
jiheunkim <ziheun54@gmail.com>
2023-08-26 13:53:55 +0900

    상단바 추가

--- skthackaton/src/App.js
+++ skthackaton/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
