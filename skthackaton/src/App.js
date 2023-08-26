4c8196cf192064d2da280f933356593d6b9782f7
jiheunkim <ziheun54@gmail.com>
2023-08-26 14:36:14 +0900

    scraps 페이지 추가

--- skthackaton/src/App.js
+++ skthackaton/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Board from './components/pages/Board';
+import Scraps from './components/pages/Scraps';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/board" element={<Board />} />
+        <Route path="/scraps" element={<Scraps />} />
 
       </Routes>
     </BrowserRouter>
