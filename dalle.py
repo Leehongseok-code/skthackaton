1719494caa711c7196f265b326ebf1e0410c13a3
jeongmyunghee <myunghee1231@naver.com>
2023-08-29 21:37:00 +0900

    fg

--- dalle.py
+++ dalle.py
@@ -6,7 +6,11 @@ class DallE():
         openai.api_key = "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"
         messages = []
         self.generated_image = ""
-
+        import urllib. request as req
+        image_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-8wcvBxN3H7xshG4VAAgkNEyu/user-c319FLewk0K5U8iaq46OPLuk/img-AIZHh8YvA61fV5Gy1moqeQBg.png?st=2023-08-29T10%3A47%3A26Z&se=2023-08-29T12%3A47%3A26Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-08-28T12%3A50%3A37Z&ske=2023-08-29T12%3A50%3A37Z&sks=b&skv=2021-08-06&sig=jRNED71RdE%2B%2BH1qjmi7fEvTwXLtI3tDRahwzYMS9X/8%3D"
+        save_path = "./test_image"
+        download_file = req.urlretrieve(image_url, save_path)
+        
     def generate(self, prompt):
         #prompt = "color this car to red https://oaidalleapiprodscus.blob.core.windows.net/private/org-8wcvBxN3H7xshG4VAAgkNEyu/user-c319FLewk0K5U8iaq46OPLuk/img-Yj05zWPnm9C2n0E1WTs5twce.png?st=2023-08-26T04%3A39%3A20Z&se=2023-08-26T06%3A39%3A20Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-08-25T17%3A04%3A42Z&ske=2023-08-26T17%3A04%3A42Z&sks=b&skv=2021-08-06&sig=phHV6wr0cqkzZIjomZdstQ7wfBmoMAscAFai5C1d%2B/w%3D"
         response = openai.Image.create(
