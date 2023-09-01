0f5a9029a82abcdef39a68ef12ad8a6d02ad699c
jiheunkim <ziheun54@gmail.com>
2023-09-02 00:16:54 +0900

    배경색상 추가

--- skthackaton/src/components/pages/SignUp.js
+++ skthackaton/src/components/pages/SignUp.js
@@ -60,15 +60,11 @@ const SignUp = () => {
 
   return (
     <>
+    <div className='App-back'>
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <br></br><br></br><br></br>
     <br></br><br></br><br></br>
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           정서랑<br></br>{text}
         </h2>
@@ -126,7 +122,7 @@ const SignUp = () => {
               onClick={handleGoogleLogin}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              구글로 로그인하기
+              Google 계정으로 로그인
             </button>
           </div>
         </form>
@@ -138,6 +134,7 @@ const SignUp = () => {
           </a>
         </p>
       </div>
+    </div>
     </div>
     <div>
       <div>
