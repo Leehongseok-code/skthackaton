3fa05ccd958f95184a44d36574ae22691e5a0680
jiheunkim <ziheun54@gmail.com>
2023-09-02 16:49:14 +0900

    메일보내기 테스트 완료

--- skthackaton/src/components/Footer.js
+++ skthackaton/src/components/Footer.js
@@ -10,9 +10,7 @@ function Footer() {
   const handleSubscribe = async () => {
     try {
       // 이메일을 백엔드로 전송
-      await axios.post(`http://115.85.181.240:8000/blockchain/sendnews/${email}`); 
-      //여기에!!! 백엔드 api 주소 집어넣어야함!!!
-      await axios.post(`http://115.85.181.240:8000/blockchain/subscribe/${email}`); //구독 시스템
+      await axios.post(`http://49.50.162.196:8000/aidoctor/newsletter/${email}`); 
       alert('Successfully subscribed!');
     } catch (error) {
       console.error('Error subscribing:', error);
