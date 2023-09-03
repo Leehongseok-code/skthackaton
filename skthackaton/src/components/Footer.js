fbf6e44331570b90b2538bfdab56ac1dd9d8162a
jiheunkim <ziheun54@gmail.com>
2023-09-03 17:34:01 +0900

    footer 이름 변경

--- skthackaton/src/components/Footer.js
+++ skthackaton/src/components/Footer.js
@@ -22,7 +22,7 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          정서랑에서 자기표현 미술치료 프로그램 정보를 받아보세요!
+          U-Dalle에서 자기표현 음악·미술치료 프로그램 정보를 받아보세요!
         </p>
         <p className='footer-subscription-text'>
           언제든지 원하실 때 구독해지 가능합니다.
@@ -37,63 +37,20 @@ function Footer() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <Button buttonStyle='btn--outline' onClick={handleSubscribe}>
+            <Button buttonStyle='btns btn--outline' onClick={handleSubscribe}>
              Subscribe
             </Button>
           </form>
         </div>
       </section>
-      <div class='footer-links'>
-        <div className='footer-link-wrapper'>
-          {/* <div class='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='../Services'>How it works</Link>
-            <Link to='../News'>News</Link>
-            <Link to='../Ranks'>Top100 Ranking</Link>
-          </div>
-          <div class='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='https://www.instagram.com/likelion_konkuk.univ/'>Instagram</Link>
-            <Link to='https://www.facebook.com/likelionkonkuk/?locale=ko_KR'>Facebook</Link>
-            <Link to='https://www.youtube.com/@likelion.official'>Youtube</Link>
-            
-          </div> */}
-        </div>
-      </div>
       <section class='social-media'>
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              정서랑
+              U-Dalle
             </Link>
           </div>
-          <small class='website-rights'>정서랑 © 2023</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='https://www.facebook.com/likelionkonkuk/?locale=ko_KR'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='https://www.instagram.com/likelion_konkuk.univ/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='https://www.youtube.com/@likelion.official'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-          </div>
+          <small class='website-rights'>유달리 © 2023</small>
         </div>
       </section>
     </div>
